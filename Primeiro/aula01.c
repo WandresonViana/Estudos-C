971698927a01a9c75c9f2b95cd748544d1e0376f
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-06 22:34:15 -0300

    Maria

--- Primeiro/aula01.c
+++ Primeiro/aula01.c
@@ -2,6 +2,9 @@
 int main(int argc, char const *argv[])
 {
     int numero = 5;
+    char nome[15] = "Maria";
+
+    printf("Meu nome é %s\n", nome);
     printf("Isso é um número %d \n", numero);
     printf("Meu primeiro programa em C!\n");    
     return 0;
