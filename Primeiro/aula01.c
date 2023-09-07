8524b36f08c0d070169eb7ed6492b421126b4762
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-06 22:23:25 -0300

    Variáveis

--- Primeiro/aula01.c
+++ Primeiro/aula01.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
+    int numero = 5;
+    printf("Isso é um número %d \n", numero);
     printf("Meu primeiro programa em C!\n");    
     return 0;
 }
