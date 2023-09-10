8aded4cc33fdd28b242c2dc3a3d638ea53ad0e16
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-10 19:06:17 -0300

    soma

--- Primeiro/matematica.c
+++ Primeiro/matematica.c
@@ -5,5 +5,8 @@ int main(int argc, char const *argv[])
     /* code */
     int num1 = 5;
     int num2 = 6;
+    int soma = num1 + num2;
+
+    printf("O total de %d mais %d e igual a %d", num1, num2, soma);
     return 0;
 }
