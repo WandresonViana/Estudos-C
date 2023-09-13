cc1866af3d3adeb8be51b5b345adae1d2e05af33
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-12 22:33:43 -0300

    Update condicoes.c

--- Primeiro/condicoes.c
+++ Primeiro/condicoes.c
@@ -8,8 +8,7 @@ int main(int argc, char const *argv[])
     char mensagem[10];
     printf("Digite um numero: \n");
     scanf("%d", &num);
-    printf("Digite uma mensagem: \n");
-    scanf("%s", &mensagem);
+   
 
     if(num == 1){
         printf("Olá, Maria\n");
