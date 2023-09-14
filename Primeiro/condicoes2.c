adc39c3f2adcbf385aa73b366c94cfd88e865d7a
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-13 23:46:58 -0300

    Erro tipos

--- Primeiro/condicoes2.c
+++ Primeiro/condicoes2.c
@@ -3,12 +3,18 @@
 int main(int argc, char const *argv[])
 {
     /* code */
-    int nota01, nota02, resultado;
+    float nota01, nota02, resultado;
     printf("Digite a primeira nota: \n");
-    scanf("%d", nota01);
-    printf("Digite a segunda nota: \n");
-    scanf("%d", nota02);
+    scanf("%f", nota01);
 
+    printf("Digite a segunda nota: \n");
+    scanf("%f", nota02);
+
+    resultado = (nota01 + nota02)/2;
+
+    if(resultado > 7){
+        printf("Aluno aprovado!");
+    }
 
     return 0;
 }
