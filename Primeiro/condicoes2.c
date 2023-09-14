4006b68b9fda2932a9f6ef9cefd4c7f6725ed045
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-13 23:37:58 -0300

    Update condicoes2.c

--- Primeiro/condicoes2.c
+++ Primeiro/condicoes2.c
@@ -3,7 +3,12 @@
 int main(int argc, char const *argv[])
 {
     /* code */
-    int nota01, nota02;
-    printf()
+    int nota01, nota02, resultado;
+    printf("Digite a primeira nota: \n");
+    scanf("%d", nota01);
+    printf("Digite a segunda nota: \n");
+    scanf("%d", nota02);
+
+
     return 0;
 }
