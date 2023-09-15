de9466a915632d7bb9549f8746e25fc878435240
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-14 23:50:06 -0300

    erro

--- Primeiro/condicoes2.c
+++ Primeiro/condicoes2.c
@@ -2,13 +2,13 @@
 
 int main(int argc, char const *argv[])
 {
-
     float nota01, nota02, resultado;
+
     printf("Digite a primeira nota: \n");
     scanf("%f", nota01);
 
-    //printf("Digite a segunda nota: \n");
-//scanf("%f", nota02);
+    printf("Digite a segunda nota: \n");
+    scanf("%f", nota02);
 
     resultado = (nota01 + nota02)/2;
 
