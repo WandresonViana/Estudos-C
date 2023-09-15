44110c35dfb6c8e274117b270ef9406ce807e197
Wandreson Viana <wandresonviana01@gmail.com>
2023-09-14 23:45:55 -0300

    Erros

--- Primeiro/condicoes2.c
+++ Primeiro/condicoes2.c
@@ -2,15 +2,15 @@
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+
     float nota01, nota02, resultado;
     printf("Digite a primeira nota: \n");
     scanf("%f", nota01);
 
-    printf("Digite a segunda nota: \n");
-    scanf("%f", nota02);
+    //printf("Digite a segunda nota: \n");
+//scanf("%f", nota02);
 
-    //resultado = (nota01 + nota02)/2;
+    resultado = (nota01 + nota02)/2;
 
     if(resultado > 7 && resultado < 8){
         printf("Aluno aprovado!");
