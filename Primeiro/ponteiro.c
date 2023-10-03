7964f2a5c3be8a7fdf82a34dac57f7e73d8b0b4d
Wandreson Viana <wandresonviana01@gmail.com>
2023-10-02 22:56:38 -0300

    Ponteiros

--- Primeiro/ponteiro.c
+++ Primeiro/ponteiro.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
     int a = 6;
-    int *p1;
+    int *p1 = NULL;
+    int *p2;
+    p1 = &a;
+    p2 = p1;
+    
 
-    *p1 = &a;
-    *p1 = 4;
-
-    printf("O ponteiro %d" + *p1);
+    printf("%p \n", *p1);
+    printf("%d \n",*p2);
     printf("Abacate");
     return 0;
 }
