69d0f35e487e3e2628ad74aba92d4fd373852615
Wandreson Viana <wandresonviana01@gmail.com>
2023-10-08 23:01:15 -0300

    Update

--- Primeiro/condicoes2.c
+++ Primeiro/condicoes2.c
@@ -6,9 +6,11 @@ int main(int argc, char const *argv[])
 
     printf("Digite a primeira nota: \n");
     scanf("%f", nota01);
+    fflush(stdin);
 
     printf("Digite a segunda nota: \n");
     scanf("%f", nota02);
+    fflush(stdin);
 
     resultado = (nota01 + nota02)/2;
 
