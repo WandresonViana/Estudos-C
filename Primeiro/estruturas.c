7b28d6b85554522a40aa2c1e1a92f612916bbe8e
Wandreson Viana <wandresonviana01@gmail.com>
2023-10-09 23:40:28 -0300

    Update estruturas.c

--- Primeiro/estruturas.c
+++ Primeiro/estruturas.c
@@ -15,6 +15,9 @@ int main(int argc, char const *argv[])
     agora.segundos = 22;
 
     printf("%i:%i:%i \n", agora.horas, agora.minutos, agora.segundos);
+
+    struct horario depois;
+    depois.horas = agora.horas + 10;
     
     return 0;
 }
