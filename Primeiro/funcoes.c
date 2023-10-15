aeba5a03a735bf4c8c15471a819401707fe437ce
Wandreson Viana <wandresonviana01@gmail.com>
2023-10-15 19:52:06 -0300

    Media

--- Primeiro/funcoes.c
+++ Primeiro/funcoes.c
@@ -5,9 +5,17 @@ int main(int argc, char const *argv[])
     int valor01, valor02;
     void imprimeMensagem(void);
 
+    imprimeMensagem();
+
     return 0;
 }
+
 void imprimeMensagem(void){
-    printf("Teste");
+    printf("Teste \n");
 }
 
+float calcularMedia(float x, float y){
+    float resultado;
+    resultado = (x + y)/2;
+    return resultado;
+}
