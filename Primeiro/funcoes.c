18488e5c8ad8e2a48af9a78e745735808f482944
Wandreson Viana <wandresonviana01@gmail.com>
2023-10-15 19:40:51 -0300

    Update funcoes.c

--- Primeiro/funcoes.c
+++ Primeiro/funcoes.c
@@ -3,7 +3,13 @@
 int main(int argc, char const *argv[])
 {
     int valor01, valor02;
-    
+    void imprimeMensagem(void);
+
+    printf("Digiet os valores");
+    scanf("%d, %d", valor01, valor02);
     return 0;
+}
+void imprimeMensagem(void){
+    printf("Abacaxi");
 }
 
