e5358bcfc77441df1d5c2c8bbe9a83750a3dc04f
Wandreson Viana <wandresonviana01@gmail.com>
2023-10-19 22:55:06 -0300

    Update funcoes2.c

--- Primeiro/funcoes2.c
+++ Primeiro/funcoes2.c
@@ -4,5 +4,13 @@ int main(int argc, char const *argv[])
 {
     int vetor[10] = {6,5,2,8,1,4,3,10,9,7};
     int i;
+    void ordenamCrescente(int vetor[], int n);
+
+    ordemCrescente(vetor, 10);
+
     return 0;
+}
+
+void ordenamCrescente(int vetor[], int n){
+
 }
